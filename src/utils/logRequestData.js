48e2028cfdbd8ef85d301f2cf929cb04bb2f8d69
Israel Ferreira <git.qodeless@outlook.com>
2021-10-29 12:32:07 -0300

    fix: application log

--- src/utils/logRequestData.js
+++ src/utils/logRequestData.js
@@ -2,7 +2,7 @@ const { logInfo } = require('../services');
 
 const logRequestData = (req, res, next) => {
     logInfo({
-        message: `Request from Host: ${req.headers?.host} | Agent: ${req.headers?.['user-agent']}`,
+        message: `Request from Host: ${req.headers?.host} | Application: ${req.headers?.application}`,
         breakLine: '\n',
     });
 
