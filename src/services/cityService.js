b82a96fc033f1ea7ddb2a051ada92bc263812adb
João Gonçalves <joao.jr.etec@gmail.com>
2021-10-29 02:24:32 -0300

    chore: add logs on weather and city services

--- src/services/cityService.js
+++ src/services/cityService.js
@@ -1,10 +1,22 @@
 const axios = require('axios');
 const { cityService } = require('../../config');
+const { logger: { info: logInfo } } = require('./loggerService');
+
+const getQuerystringFromObject = obj => Object.keys(obj)
+    .filter(key => key !== 'key')
+    .map(key => `${key}=${obj[key]}`)
+    .join('&');
+
 
 const makeRequest = async requestConfig => {
     const url = cityService.url;
 
+    const querystring = getQuerystringFromObject(requestConfig.params);
+    logInfo(`Calling weather external api: ${url}?${querystring}...`);
+
     const { data } = await axios.get(url, requestConfig);
+
+    logInfo('Get City Id From City Name operation was executed successfully');
 
     return data;
 };
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,10 +1,33 @@
 const axios = require('axios');
+const { logger: { info: logInfo } } = require('./loggerService');
 const { weatherService } = require('../../config');
 
-const makeRequest = async requestConfig => {
+const splitStringByCapitalLetters = s => {
+    if (typeof s !== 'string') return '';
+    return s.split(/(?=[A-Z]+[^A-Z]?)/);
+};
+
+const capitalizeFirstLetter = s => {
+    if (typeof s !== 'string') return '';
+    return s.charAt(0).toUpperCase() + s.slice(1);
+};
+
+const getFunctionDescription = functionName => capitalizeFirstLetter(splitStringByCapitalLetters(functionName).join(' '));
+
+const getQuerystringFromObject = obj => Object.keys(obj)
+    .filter(key => key !== 'key')
+    .map(key => `${key}=${obj[key]}`)
+    .join('&');
+
+const makeRequest = async (requestConfig, functionName) => {
     const url = weatherService.url;
 
+    const querystring = getQuerystringFromObject(requestConfig.params);
+    logInfo(`Calling weather external api: ${url}?${querystring}...`);
+
     const { data } = await axios.get(url, requestConfig);
+
+    logInfo(`${getFunctionDescription(functionName)} operation was executed successfully`);
 
     return data;
 };
@@ -17,7 +40,7 @@ const getWeatherByCityName = async cityName => {
         },
     };
 
-    return makeRequest(requestConfig);
+    return makeRequest(requestConfig, 'getWeatherByCityName');
 };
 
 const getWeatherByLatitudeAndLongitude = async (latitude, longitude) => {
@@ -29,7 +52,7 @@ const getWeatherByLatitudeAndLongitude = async (latitude, longitude) => {
         },
     };
 
-    return makeRequest(requestConfig);
+    return makeRequest(requestConfig, 'getWeatherByLatitudeAndLongitude');
 };
 
 const getWeatherByCityId = async cityId => {
@@ -40,7 +63,7 @@ const getWeatherByCityId = async cityId => {
         },
     };
 
-    return makeRequest(requestConfig);
+    return makeRequest(requestConfig, 'getWeatherByCityId');
 };
 
 module.exports = {
